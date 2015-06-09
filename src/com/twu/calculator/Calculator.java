be4ad802ed6a973480859afd026b43d2cdd3620c
H4rryp0tt3r <nagesh.podilapu@gmail.com>
2015-06-09 12:53:14 +0530

    Jaswanth/Nagesh - Added spec for add command

--- src/com/twu/calculator/Calculator.java
+++ src/com/twu/calculator/Calculator.java
@@ -1,7 +1,18 @@
 package com.twu.calculator;
 
 public class Calculator {
-    public double processCommand(String s) {
-        return 0;
+
+    private double result;
+
+    public double processCommand(String command) {
+        String[] splitCommand = command.split(" ");
+        String operation = splitCommand[0];
+        double operand = Double.parseDouble(splitCommand[1]);
+        switch (operation) {
+            case "add":
+                result += operand;
+                break;
+        }
+        return result;
     }
 }
---
+++ test/com/twu/calculator/CalculatorTest.java
@@ -0,0 +1,37 @@
+package com.twu.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+
+    @Test
+    public void shouldReturnZeroWhenZeroIsAddedAsInitialCommand() {
+        Calculator calculator = new Calculator();
+
+        double actualResult = calculator.processCommand("add 0");
+
+        assertThat(actualResult, is(0.0));
+    }
+
+    @Test
+    public void shouldReturnCorrectValueWhenAValueIsAddedAsInitialCommand() {
+        Calculator calculator = new Calculator();
+
+        double actualResult = calculator.processCommand("add 10");
+
+        assertThat(actualResult, is(10.0));
+    }
+
+    @Test
+    public void shouldReturnCorrectValueWhenAValueIsAddedAsSecondCommand() {
+        Calculator calculator = new Calculator();
+        calculator.processCommand("add 10");
+        
+        double actualResult = calculator.processCommand("add 20");
+
+        assertThat(actualResult, is(30.0));
+    }
+}
